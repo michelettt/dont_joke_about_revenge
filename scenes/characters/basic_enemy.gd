02bcb25d37772eaac5e981e3f750d84242d41995
William <162297723+WilliTini@users.noreply.github.com>
2025-06-26 19:59:53 +0200

    enemy_quasifatto

---
+++ scenes/characters/basic_enemy.gd
@@ -0,0 +1,9 @@
+class_name BasicEnemy
+extends Character
+
+@export var player : Player
+
+func handle_input() -> void:
+	if player != null:
+		var direction = (player.position - position).normalized()
+		velocity = direction * speed
